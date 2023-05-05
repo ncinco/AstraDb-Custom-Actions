ac034678f11c95616f7b4f3cfd178a355fbe41f3
ncinco <ncinco@gmail.com>
2023-05-05 04:54:56 +0000

    Commit

---
+++ DataStax.DevOps.ViewModels/Database/DatabaseInfo.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace DataStax.DevOps.ViewModels
+{
+    public class DatabaseInfo
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("orgId")]
+        public string OrgId { get; set; }
+
+        [JsonProperty("ownerId")]
+        public string OwnerId { get; set; }
+
+        [JsonProperty("info")]
+        public Info Info { get; set; }
+
+        [JsonProperty("creationTime")]
+        public DateTime CreationTime { get; set; }
+
+        [JsonProperty("terminationTime")]
+        public DateTime TerminationTime { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using AstraDb_Custom_Actions.HttpRequests;
+using CommandLine;
+using DataStax.DevOps.ViewModels;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace AstraDb_Custom_Actions
+{
+    public class Options
+    {
+        [Option('t', "token", Required = true, HelpText = "AstraDB token.")]
+        public string Token { get; set; }
+
+        [Option('d', "database", Required = true, HelpText = "Database name.")]
+        public string Database { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+
+            Parser.Default.ParseArguments<Options>(args)
+               .WithParsed(o =>
+               {
+                   options = o;
+               });
+
+            Console.WriteLine($"Current Arguments: -t {options.Token}");
+
+            var restClientOptions = new RestClientOptions("https://api.astra.datastax.com");
+            var restClient = new RestClient(restClientOptions);
+            restClient.AddDefaultHeader("Authorization", $"Bearer {options.Token}");
+
+            var listResult = restClient.GetJson<List<DatabaseInfo>>("v2/databases");
+
+            var found = listResult.Any(x => string.Compare(x.Info.Name, options.Database, StringComparison.InvariantCultureIgnoreCase) == 0);
+
+            if (!found)
+            {
+                var request = new RestRequest("v2/databases");
+                var requestBody = new CreateDatabaseRequest
+                {
+                    Name = "cicd_database",
+                    Keyspace = "accounts",
+                    CloudProvider = "AZURE",
+                    Tier = "serverless",
+                    CapacityUnits = 1,
+                    Region = "australiaeast",
+                    User = "db_admin",
+                    Password = "db_admin"
+                };
+
+                request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+                var createResult = restClient.Post(request);
+            }
+        }
+    }
+}
