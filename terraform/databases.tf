78cb74fe35a4d9ca62857cffbb0e67424925c97b
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-08 04:26:51 +0000

    Update databases.tf

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -1,12 +1,12 @@
-resource "astra_database" "accounts_dev_tf" {
-  name           = "accounts_dev_tf"
+resource "astra_database" "accounts_terraform" {
+  name           = "accounts_terraform"
   keyspace       = "accounts"
   cloud_provider = "azure"
   regions        = ["australiaeast"] //, "westeurope"]
 }
 
-//resource "astra_database" "transactions_dev_tf" {
-//  name           = "transactions_dev_tf"
+//resource "astra_database" "transactions_terraform" {
+//  name           = "transactions_terraform"
 //  keyspace       = "transactions"
 //  cloud_provider = "azure"
 //  regions        = ["australiaeast", "westeurope"]
