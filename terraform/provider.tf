758ca1a76cb3d49fee3b0b0b78cab795771b8931
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-08 01:00:50 +0000

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    astra = {
+      source  = "datastax/astra"
+      version = ">=1.0.0"
+    }
+  }
+}
+
+variable "token" {}
+
+provider "astra" {
+  // This can also be set via ASTRA_API_TOKEN environment variable.
+  token = var.token
+}
