91630a263e4365a47f70452fac2661cbea8f2a78
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-08 04:34:42 +0000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,3 +13,27 @@ provider "astra" {
   // This can also be set via ASTRA_API_TOKEN environment variable.
   token = var.token
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "Terraform_State"
+      storage_account_name = "astradbstate"
+      container_name       = "development"
+      key                  = "astradb_terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "australiaeast"
+}
