36fb49d961271525a3043c3c0f38c52773258e21
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-08 00:57:30 +0000

    Delete backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstates"
-    storage_account_name = "kstfstateaccountv5"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
