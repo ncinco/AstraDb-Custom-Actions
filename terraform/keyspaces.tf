660222d4eb6d2e7b3e3e31724bd7e54a33f127f7
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-09 04:21:26 +0000

    Update keyspaces.tf

--- terraform/keyspaces.tf
+++ terraform/keyspaces.tf
@@ -1,4 +1,9 @@
 resource "astra_keyspace" "transactions" {
+  name        = "accounts"
+  database_id = astra_database.accounts_terraform.id
+}
+
+resource "astra_keyspace" "transactions" {
   name        = "transactions"
   database_id = astra_database.accounts_terraform.id
 }
