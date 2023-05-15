47db19bf4f2e9f931eac42e44ebadfea8744b744
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-15 01:43:50 +0000

    Update 0001_script.sql

--- accounts/01_schema/0001_script.sql
+++ accounts/01_schema/0001_script.sql
@@ -1,11 +1,4 @@
-CREATE KEYSPACE ninokp
-WITH replication = {
-  'class': 'NetworkTopologyStrategy',
-  'australiaeast': '3'
-}
-AND durable_writes = true;
-
-CREATE TABLE ninokp.ninotable
+CREATE TABLE accounts.ninotable
 (
   id UUID PRIMARY KEY,
   last_name TEXT,
