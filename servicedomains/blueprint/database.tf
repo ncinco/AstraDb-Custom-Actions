259b1ed7fffa12f410013618330aec04081df205
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-18 01:50:50 +0000

    Update database.tf

--- servicedomains/blueprint/database.tf
+++ servicedomains/blueprint/database.tf
@@ -1,5 +1,4 @@
 resource "astra_database" "blueprint" {
-  count          = "${var.environment == "dev" ? 1 : 0}"
   name           = "blueprint"
   keyspace       = "blueprint"
   cloud_provider = var.environment == "dev" ? "azure" : "gcp"
