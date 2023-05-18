f53ce71a41a851bade39090d61d8a71c4ccb4b3a
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-18 04:07:07 +0000

    Update variables.tf

--- servicedomains/modules/astradb/variables.tf
+++ servicedomains/modules/astradb/variables.tf
@@ -1,16 +1,3 @@
 variable "token" {}
 
 variable "environment" {}
-
-variable "database" {}
-
-# backend - azure blob storage
-variable "resourcegroup" {
-  description = "The resource group where the storage account is."
-}
-variable "storageaccountname" {
-  description = "The name of the storage account."
-}
-variable "storagecontainer" {
-  description = "The container name where the terraform stage to be stored."
-}
