22888383a3db87620dd455dea9e3b79feca1146d
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-19 01:30:17 +0000

    Create tables.tf

---
+++ terraform/modules/astradb/tables.tf
@@ -0,0 +1,22 @@
+resource "astra_table" "executed_scripts" {
+  table               = "executed_scripts"
+  keyspace            = var.database
+  database_id         = astra_database.astradb.id
+  region              = "australiaeast"
+  clustering_columns  = "a:b"
+  partition_keys      = "c:d"
+  column_definitions= [
+    {
+      Name: "execution_id"
+      TypeDefinition: "uuid"
+    },
+    {
+      Name: "execution_datetime"
+      TypeDefinition: "timestamp"
+    },
+    {
+      Name: "script_name"
+      TypeDefinition: "text"
+    },
+  ]
+}
