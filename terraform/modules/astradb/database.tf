eeb21156cf7c69d396f9db8a23a7dca8273209ad
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-19 01:40:44 +0000

    Update database.tf

--- terraform/modules/astradb/database.tf
+++ terraform/modules/astradb/database.tf
@@ -2,5 +2,5 @@ resource "astra_database" "astradb" {
   name           = var.database
   keyspace       = var.database
   cloud_provider = var.environment == "dev" ? "azure" : "gcp"
-  regions        = var.environment == "dev" ? ["australiaeast", "westeurope"] : ["us-east1"]
+  regions        = var.environment == "dev" ? ["australiaeast"] : ["us-east1"] #"westeurope"
 }
