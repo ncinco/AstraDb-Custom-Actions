87ac5da11ff61a4bed91c5954e9a28fff1c96572
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-25 02:29:11 +0000

    Update main.tf

--- servicedomains/accounts/main.tf
+++ servicedomains/accounts/main.tf
@@ -13,6 +13,11 @@ terraform {
   backend "azurerm" {}
 }
 
+provider "astra" {
+  // This can also be set via ASTRA_API_TOKEN environment variable.
+  token = var.token
+}
+
 module "astradb" {
   source         = "../../terraforms/modules/astradb"
   token          = var.token
