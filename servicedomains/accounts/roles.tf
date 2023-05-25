c1155486d984150ac1012fa26072beac8a894268
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-25 02:45:35 +0000

    Update roles.tf

--- servicedomains/accounts/roles.tf
+++ servicedomains/accounts/roles.tf
@@ -5,8 +5,8 @@ resource "astra_role" "api_read_account" {
   resources = [
     # The following 3 resources are needed and wildcarded to associate the role to all dbs
     "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id}",
-    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id:keyspace:*}",
-    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id:keyspace:*:table:*}"
+    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id}:keyspace:*",
+    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id}:keyspace:*:table:*"
   ]
   policy = [
     "db-table-select",
