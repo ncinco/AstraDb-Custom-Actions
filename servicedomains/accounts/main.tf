641624873bd96f95e3ecd23a82ddd705ee2a6697
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-25 02:25:40 +0000

    Update main.tf

--- servicedomains/accounts/main.tf
+++ servicedomains/accounts/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    astra = {
+      source  = "datastax/astra"
+      version = "=2.1.15"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.55.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
 module "astradb" {
   source         = "../../terraforms/modules/astradb"
   token          = var.token
