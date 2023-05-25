074b90acd5fe37d63027c5660ee596463ed42707
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-25 02:44:37 +0000

    Update roles.tf

--- servicedomains/accounts/roles.tf
+++ servicedomains/accounts/roles.tf
@@ -1,1 +1,16 @@
-
+resource "astra_role" "api_read_account" {
+  role_name   = "api_read_account"
+  description = "Role for read and write operations to tables."
+  effect      = "allow"
+  resources = [
+    # The following 3 resources are needed and wildcarded to associate the role to all dbs
+    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id}",
+    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id:keyspace:*}",
+    "drn:astra:org:f9f4b1e0-4c05-451e-9bba-d631295a7f73:db:{astra_database.astradb.id:keyspace:*:table:*}"
+  ]
+  policy = [
+    "db-table-select",
+    "db-cql",
+    "db-rest"
+  ]
+}
