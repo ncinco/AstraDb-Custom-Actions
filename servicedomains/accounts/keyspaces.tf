e34244eeac7cc49f52d556386d5f44377fafc08a
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-25 01:57:59 +0000

    Create keyspaces.tf

---
+++ servicedomains/accounts/keyspaces.tf
@@ -0,0 +1,19 @@
+resource "astra_keyspace" "savings" {
+  name        = "savings"
+  database_id = astra_database.astradb.id
+}
+
+resource "astra_keyspace" "checkings" {
+  name        = "checkings"
+  database_id = astra_database.astradb.id
+}
+
+resource "astra_keyspace" "moneymarket" {
+  name        = "moneymarket"
+  database_id = astra_database.astradb.id
+}
+
+resource "astra_keyspace" "deposit" {
+  name        = "deposit"
+  database_id = astra_database.astradb.id
+}
