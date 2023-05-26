b1ffc35945f884bb8cb415042462d35811057927
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-26 00:22:02 +0000

    Update roles.tf

--- organisations/accounts/roles.tf
+++ organisations/accounts/roles.tf
@@ -5,8 +5,19 @@ resource "astra_role" "api_read_accounts" {
   resources = [
     "drn:astra:org:${var.organisationid}",
     "drn:astra:org:${var.organisationid}:db:${var.astradbid}",
-    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:*",
-    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:*:table:*"
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:system_schema:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:system:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:system_virtual_schema:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:accounts",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:accounts:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:deposit",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:deposit:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:checkings",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:checkings:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:moneymarket",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:moneymarket:table:*",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:savings",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:savings:table:*"
   ]
   policy = [
     "db-table-select",
