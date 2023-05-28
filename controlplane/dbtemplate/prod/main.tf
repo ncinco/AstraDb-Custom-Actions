e874e85494bc0027769e28c29a2fefab0048b2ca
nino.cinco <nino.cinco@gmail.com>
2023-05-28 21:31:26 +0000

    remove backend config

--- controlplane/dbtemplate/prod/main.tf
+++ controlplane/dbtemplate/prod/main.tf
@@ -10,7 +10,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+	resource_group_name  = "Terraform_State"
+	storage_account_name = "astradbstate"
+	container_name       = "prod"
+	key                  = "[dbtemplate]_prod.tfstate"
+  }
 }
 
 provider "astra" {
