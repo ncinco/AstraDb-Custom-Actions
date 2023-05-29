d7f5301102c411e7712c974f9b6f3eced165567a
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-29 00:18:28 +0000

    Create tables.tf

---
+++ dataplane/kiwisaver/tables.tf
@@ -0,0 +1,30 @@
+resource "astra_table" "executed_scripts" {
+  table                = "executed_scripts"
+  keyspace             = "utilities"
+  database_id          = astra_database.astradb.id
+  region               = var.environment == "dev" ? "australiaeast" : "us-east1"
+  clustering_columns   = "execution_id:execution_timestamp"
+  partition_keys       = "folder:script_name:triggered_by"
+  column_definitions   = [
+    {
+      Name: "folder"
+      TypeDefinition: "text"
+    },
+    {
+      Name: "script_name"
+      TypeDefinition: "text"
+    },
+    {
+      Name: "triggered_by"
+      TypeDefinition: "text"
+    },
+    {
+      Name: "execution_id"
+      TypeDefinition: "uuid"
+    },
+    {
+      Name: "execution_timestamp"
+      TypeDefinition: "timestamp"
+    }
+  ]
+}
