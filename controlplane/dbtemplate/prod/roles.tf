d1c0010862871ab7718c48cb1779fed614c22809
ncinco <32296929+ncinco@users.noreply.github.com>
2023-06-08 02:19:35 +0000

    Update roles.tf

--- controlplane/dbtemplate/prod/roles.tf
+++ controlplane/dbtemplate/prod/roles.tf
@@ -1,6 +1,6 @@
-resource "astra_role" "api_prod_[dbtemplate]" {
-  role_name   = "ApiTeam Prod - [dbtemplate]"
-  description = "ApiTeam Prod role for read operations to tables."
+resource "astra_role" "api_read_[dbtemplate]" {
+  role_name   = "API Read - [dbtemplate]"
+  description = "Role for read and write operations to tables."
   effect      = "allow"
   resources = [
     "drn:astra:org:${var.organisationid}",
@@ -12,5 +12,47 @@ resource "astra_role" "api_prod_[dbtemplate]" {
     "db-table-select",
     "db-cql",
     "db-rest"
+  ]
+}
+
+resource "astra_role" "api_write_[dbtemplate]" {
+  role_name   = "API Write - [dbtemplate]"
+  description = "Role for read and write operations to tables."
+  effect      = "allow"
+  resources = [
+    "drn:astra:org:${var.organisationid}",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:[dbtemplate]",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:[dbtemplate]:table:*"
+  ]
+  policy = [
+    "db-table-select",
+    "db-table-modify",
+    "db-cql",
+    "db-rest"
+  ]
+}
+
+resource "astra_role" "api_[dbtemplate]" {
+  role_name   = "ApiTeam - [dbtemplate]"
+  description = "AppTeam role for read and write operations to tables."
+  effect      = "allow"
+  resources = [
+    "drn:astra:org:${var.organisationid}",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:[dbtemplate]",
+    "drn:astra:org:${var.organisationid}:db:${var.astradbid}:keyspace:[dbtemplate]:table:*"
+  ]
+  policy = [
+    "db-table-select",
+    "db-table-modify",
+    "db-table-describe",
+
+    "db-keyspace-describe",
+
+    "db-cql",
+    "db-rest",
+
+    "org-db-view"
   ]
 }
