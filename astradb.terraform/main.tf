061e6198c15f2f404bded38bb1162c7b93058be6
ncinco <32296929+ncinco@users.noreply.github.com>
2023-06-13 01:29:56 +0000

    Update main.tf

--- astradb.terraform/main.tf
+++ astradb.terraform/main.tf
@@ -6,7 +6,12 @@ terraform {
     }
   }
   
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "Terraform_State"
+    storage_account_name = "astradbstate"
+    container_name       = "dev"
+    key                  = "control_plant.tfstate"
+  }
 }
 
 provider "astra" {
