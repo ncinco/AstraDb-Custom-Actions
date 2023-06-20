3b750e7769aac0332156191fb6fecc9a26f7d57f
ncinco <32296929+ncinco@users.noreply.github.com>
2023-06-20 02:13:58 +0000

    Update rotateAccessToken.py

--- token_rotation/rotateAccessToken.py
+++ token_rotation/rotateAccessToken.py
@@ -61,6 +61,8 @@ for secretProperty in secretProperties:
 
     # create new token first before deleting the old one
     try:
+      payload = f'{"roles" : {roles}}'
+      print(payload)
       tokensResponse = requests.post(datastaxControlPlaneTokenUrl, data=json.dumps(roles), headers=headers, timeout=30)
       tokensResponse.raise_for_status()
     except requests.exceptions.HTTPError as error:
